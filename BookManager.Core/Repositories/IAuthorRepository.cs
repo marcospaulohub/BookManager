22c111f1cfaa11f2e105c8650d2487f23fd8a68f
Marcos Paulo <marcos.poo@gmail.com>
2025-05-12 16:27:12 -0300

    feat: Criando interfaces de repository.

---
+++ BookManager.Core/Repositories/IAuthorRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookManager.Core.Entities;
+
+namespace BookManager.Core.Repositories
+{
+    public interface IAuthorRepository : IRepository<Author>
+    {
+        Author GetByName(string name);
+    }
+}
---
+++ BookManager.Core/Repositories/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookManager.Core.Entities;
+
+namespace BookManager.Core.Repositories
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+        Book? GetByTitle(string title);
+        Book? GetByIsbn(string isbn);
+    }
+}
