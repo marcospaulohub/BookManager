f4add5ab64db762b85e282102824d636aad04c0a
Marcos Paulo <marcos.poo@gmail.com>
2025-05-13 15:18:20 -0300

    feat: Criação de ViewModel e InputModel para a entidade Loan.

---
+++ BookManager.App/Models/Loans/CreateLoanInputModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BookManager.App.Models.Loans
+{
+    public class CreateLoanInputModel
+    {
+        public CreateLoanInputModel(int userId, int bookId)
+        {
+            UserId = userId;
+            BookId = bookId;
+        }
+
+        public int UserId { get; private set; }
+        public int BookId { get; private set; }
+    }
+}
---
+++ BookManager.App/Models/Loans/LoanViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BookManager.Core.Entities;
+
+namespace BookManager.App.Models.Loans
+{
+    public class LoanViewModel
+    {
+        public LoanViewModel(int id, int userId, int bookId, DateTime loanDate, DateTime returnDate)
+        {
+            Id = id;
+            UserId = userId;
+            BookId = bookId;
+            LoanDate = loanDate;
+            ReturnDate = returnDate;
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+        public static LoanViewModel? FromEntity(Loan entity)
+            => new(entity.Id,
+                entity.UserId,
+                entity.BookId,
+                entity.LoanDate,
+                entity.ReturnDate);
+    }
+}
