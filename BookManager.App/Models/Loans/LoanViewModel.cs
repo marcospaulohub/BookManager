8c26861211c994479c204c571dd534adb495a566
Marcos Paulo <marcos.poo@gmail.com>
2025-05-14 05:15:10 -0300

    feat: Adicionando o Nome do usuário e o nome do livro no ViewModel.

--- BookManager.App/Models/Loans/LoanViewModel.cs
+++ BookManager.App/Models/Loans/LoanViewModel.cs
@@ -4,25 +4,31 @@ namespace BookManager.App.Models.Loans
 {
     public class LoanViewModel
     {
-        public LoanViewModel(int id, int userId, int bookId, DateTime loanDate, DateTime returnDate)
+        public LoanViewModel(int id, int userId, string userName, int bookId, string bookTitle, DateTime loanDate, DateTime returnDate)
         {
             Id = id;
             UserId = userId;
+            UserName = userName;
             BookId = bookId;
+            BookTitle = bookTitle;
             LoanDate = loanDate;
             ReturnDate = returnDate;
         }
 
         public int Id { get; set; }
         public int UserId { get; set; }
+        public string UserName { get; set; }
         public int BookId { get; set; }
+        public string BookTitle { get; set; }
         public DateTime LoanDate { get; set; }
         public DateTime ReturnDate { get; set; }
 
         public static LoanViewModel? FromEntity(Loan entity)
             => new(entity.Id,
                 entity.UserId,
+                entity.User.Name,
                 entity.BookId,
+                entity.Book.Title,
                 entity.LoanDate,
                 entity.ReturnDate);
     }
