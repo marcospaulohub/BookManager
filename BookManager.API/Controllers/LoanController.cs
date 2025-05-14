77e3d2179587a50154f70ff1d29c806632ab92a3
Marcos Paulo <marcos.poo@gmail.com>
2025-05-14 05:05:42 -0300

    feat: Criação do LoanController.

---
+++ BookManager.API/Controllers/LoanController.cs
@@ -0,0 +1,38 @@
+﻿using BookManager.App.Models.Loans;
+using BookManager.App.Services.Loans;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManager.API.Controllers
+{
+    [Route("api/Loan")]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        private readonly ILoanService _loanService;
+
+        public LoanController(ILoanService loanService)
+        {
+            _loanService = loanService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateLoanInputModel model)
+        {
+            var result = _loanService.Insert(model);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data}, model);
+        }
+
+        [HttpGet("PorId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _loanService.GetById(id);
+
+            if(!result.IsSuccess)
+            {
+                return NotFound(result.Message);
+            }
+            return Ok(result);
+        }
+    }
+}
