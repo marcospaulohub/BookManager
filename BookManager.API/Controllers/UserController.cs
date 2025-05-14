d0eae6ec047148fc0b6e4c018a17f9d8069821aa
Marcos Paulo <marcos.poo@gmail.com>
2025-05-14 05:05:29 -0300

    feat: Criação do UserController.

---
+++ BookManager.API/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using BookManager.App.Models.Users;
+using BookManager.App.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManager.API.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateUserInputModel model)
+        {
+            var result = _userService.Insert(model);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data}, model);
+        }
+
+        [HttpGet("PorId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _userService.GetById(id);
+
+            if(!result.IsSuccess)
+            {
+                return NotFound(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
