5391ac6ed4418a7210c84fc3dfaf245180ba6dc2
Marcos Paulo <marcos.poo@gmail.com>
2025-05-15 06:21:44 -0300

    feat: Resolver o problema de recursividade entre as ViewModels

---
+++ BookManager.App/Models/Authors/AuthorSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookManager.Core.Entities;
+
+namespace BookManager.App.Models.Authors
+{
+    public class AuthorSummaryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static AuthorSummaryViewModel FromEntity(Author author) =>
+            new() { Id = author.Id, Name = author.Name };
+    }
+}
---
+++ BookManager.App/Models/Books/BookSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookManager.Core.Entities;
+
+namespace BookManager.App.Models.Books
+{
+    public class BookSummaryViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public static BookSummaryViewModel FromEntity(Book book) =>
+            new() { Id = book.Id, Title = book.Title };
+    }
+}
