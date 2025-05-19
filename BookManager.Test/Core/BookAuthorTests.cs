efdd4274e1ac6d6e1ceb49492223e8b6a7cc3669
Marcos Paulo <marcos.poo@gmail.com>
2025-05-19 04:04:02 -0300

    test: Adiciona o BookAuthorTests.

---
+++ BookManager.Test/Core/BookAuthorTests.cs
@@ -0,0 +1,18 @@
+﻿using BookManager.Test.Fakes;
+
+namespace BookManager.Test.Core
+{
+    public class BookAuthorTests
+    {
+        [Fact]
+        public void BookAuthor_Should_Have_Book_And_Author()
+        {
+            // Testar relação BookAuthor
+
+            var bookAuthor = FakeDataHelper.CreateFakeBookAuthor();
+
+            Assert.NotNull(bookAuthor.Book);
+            Assert.NotNull(bookAuthor.Author);
+        }
+    }
+}
