4910042fb64dd9e137f40fcaee50133b6572362a
Marcos Paulo <marcos.poo@gmail.com>
2025-05-20 04:35:01 -0300

    feat: Criando o Validation de Author.

---
+++ BookManager.App/Validators/Authors/CreateAuthorInputModelValidator.cs
@@ -0,0 +1,29 @@
+﻿using BookManager.App.Models.Authors;
+using FluentValidation;
+
+namespace BookManager.App.Validators.Authors
+{
+    public class CreateAuthorInputModelValidator : AbstractValidator<CreateAuthorInputModel>
+    {
+        public CreateAuthorInputModelValidator()
+        {
+            RuleFor(a => a.Name)
+                .NotEmpty()
+                    .WithMessage("O nome do autor é obrigatório.")
+                .MinimumLength(3)
+                    .WithMessage("O nome deve ter pelo menos 3 caracteres.")
+                .MaximumLength(100)
+                    .WithMessage("O nome deve ter no máximo 100 caracteres.");
+
+            RuleFor(a => a.Nationality)
+                .Must(n => n == "Brasileiro" || n == "Estrangeiro")
+                .WithMessage("Nacionalidade deve ser 'Brasileiro' ou 'Estrangeiro'.");
+
+            RuleFor(a => a.BirthDate)
+                .LessThanOrEqualTo(DateTime.Today)
+                    .WithMessage("A data de nascimento não pode ser futura.")
+                .Must(a => a < DateTime.Now.AddYears(-18))
+                    .WithMessage("O Autor precisar ser maior de idade.");
+        }
+    }
+}
