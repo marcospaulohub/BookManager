b7055bc323b98cc941ffdae9ed61018ca27b8b6c
Marcos Paulo <marcos.poo@gmail.com>
2025-06-04 04:39:23 -0300

    feat: Adiciona a entidade BookCategory.

---
+++ BookManager.Core/Entities/BookCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace BookManager.Core.Entities
+{
+    public class BookCategory : BaseEntity
+    {
+        public BookCategory() : base() { }
+        public BookCategory(Book book, Category category) : base()
+        {
+            Book = book;
+            Category = category;
+
+            book.Categories.Add(this);
+            category.Books.Add(this);
+        }
+
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
