620b858215561dcd4feed75a2622f6aa8d943030
Marcos Paulo <marcos.poo@gmail.com>
2025-06-04 04:40:31 -0300

    feat: Adiciona um novo construtor para receber uma lista de Category.

--- BookManager.Core/Entities/Book.cs
+++ BookManager.Core/Entities/Book.cs
@@ -25,10 +25,26 @@ namespace BookManager.Core.Entities
                 .ToList();
         }
 
+        public Book(string title, string iSBN, DateTime publicationDate, List<Author> authors, List<Category> categories) : base()
+        {
+            Title = title;
+            ISBN = iSBN;
+            PublicationDate = publicationDate;
+            
+            Authors = authors
+                .Select(a => new BookAuthor { Author = a })
+                .ToList();
+
+            Categories = categories
+                .Select(c => new BookCategory { Category = c })
+                .ToList();
+        }
+
         public string Title { get; set; }
         public string? ISBN { get; set; }
         public DateTime? PublicationDate { get; set; }
         public List<BookAuthor> Authors { get; set; } = [];
+        public List<BookCategory> Categories { get; set; } = [];
         public List<Loan> Loans { get; set; } = [];
     }
 }
