1b41800df7b7602dd213c46dc5c0ac8c2a1ddc26
Marcos Paulo <marcos.poo@gmail.com>
2025-06-04 04:39:01 -0300

    feat: Adiciona a entidade Category.

---
+++ BookManager.Core/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BookManager.Core.Entities
+{
+    public class Category : BaseEntity
+    {
+        public Category(string name, string? description)
+        {
+            Name = name;
+            Description = description;
+            
+            IsActive = true;
+        }
+
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+        public List<BookCategory> Books { get; set; } = [];
+    }
+}
