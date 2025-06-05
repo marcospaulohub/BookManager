13930cd928f017c288a2d67b289dc5243b15fe46
Marcos Paulo <marcos.poo@gmail.com>
2025-06-05 04:45:28 -0300

    feat: Criação do CategoryController.

---
+++ BookManager.API/Controllers/CategoryController.cs
@@ -0,0 +1,79 @@
+﻿using BookManager.App.Models.Categories;
+using BookManager.App.Services.Categories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManager.API.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateCategoryInputModel model)
+        {
+            var result = _categoryService.Insert(model);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data }, model);
+        }
+
+        [HttpGet("PorId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _categoryService.GetById(id);
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, UpdateCategoryInputModel model)
+        {
+            var result = _categoryService.Update(id, model);
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _categoryService.Delete(id);
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
