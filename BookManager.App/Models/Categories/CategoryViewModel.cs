55ae348b9cc3803f5bc0044e1fe037ce0eb91ea8
Marcos Paulo <marcos.poo@gmail.com>
2025-06-05 04:44:31 -0300

    feat: Criação do CategoryViewModel.

---
+++ BookManager.App/Models/Categories/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BookManager.Core.Entities;
+
+namespace BookManager.App.Models.Categories
+{
+    public class CategoryViewModel
+    {
+        public CategoryViewModel(int id, string name, string? description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public static CategoryViewModel? FromEntity(Category entity)
+            => new(entity.Id, entity.Name, entity.Description);
+    }
+}
