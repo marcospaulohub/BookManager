398a7293c37693d856d53b79295a879926d7061d
Marcos Paulo <marcos.poo@gmail.com>
2025-06-05 15:52:54 -0300

    feat: Adiciona InputModel de Update para Loan.

---
+++ BookManager.App/Models/Loans/UpdateLoanInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BookManager.App.Models.Loans
+{
+    public class UpdateLoanInputModel
+    {
+        public int UserId { get; private set; }
+        public int BookId { get; private set; }
+    }
+}
